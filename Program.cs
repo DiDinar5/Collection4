1d0905737e166de2c2f7122d6f776e8a43d6439c
Dinar <dinar-wp9@mail.ru>
2022-12-10 12:59:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Collectiontest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            string txt = File.ReadAllText(x);
+
+            // Использование регулярных выражений для замены символов
+            // это не буквы или числа с пробелами.
+            Regex reg_exp = new Regex("[^а-яА-Я0-9]");
+            txt = reg_exp.Replace(txt, " ");
+
+            // Разделить текст на слова.
+            string[] words = txt.Split(
+                new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            // Используйте LINQ для получения уникальных слов.
+            var word_query =
+                (from string word in words
+                 orderby word
+                 select word).Distinct();
+
+            // Отображение результата.
+            string[] result = word_query.ToArray();
+            Array.Sort(result);
+            foreach (string s in result)
+                if (s.Length > 18)
+                    Console.WriteLine(s.Length + s);
+        }
+    }
+}
